ac8172792b4efded4936d7cb1300caeba3059f11
Robin Steinhauser <rbn2208@gmail.com>
2021-02-25 20:33:25 +0100

    Finished homework

--- src/lib/string.js
+++ src/lib/string.js
@@ -1,3 +1,16 @@
 export function commaSeparatedToArray(text){
     return text.split(',').map(word => word.trim())
 }
+
+export function sortArrayAlphabetically(text){
+    return text.split(',').sort()
+}
+
+export function reduceDublicates(text){
+    const newText = [...new Set(text)]
+    return newText
+}
+
+export function transformTitelcase(...text){
+    return text.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+}
--- src/lib/string.spec.js
+++ src/lib/string.spec.js
@@ -1,25 +1,50 @@
-import { commaSeparatedToArray } from "./string"
+import { commaSeparatedToArray, transformTitelcase } from "./string"
+import { sortArrayAlphabetically } from "./string"
+import { reduceDublicates } from "./string"
+
+
 
 describe('lib/string', () => {
     describe('commaSeparatedToArray', () => {
       it('returns ["a", "b"] for text "a,b"', () => {
           const result = commaSeparatedToArray('a,b')
-          expect(result).toStrictEqual(['a', 'b'])
+          expect(result).toEqual(['a', 'b'])
       })  
       it('returns ["a", "b"] for text "  a , b "', () => {
           const result = commaSeparatedToArray(' a , b  ')
-          expect(result).toStrictEqual(['a', 'b'])
+          expect(result).toEqual(['a', 'b'])
       })
       it('returns ["a b", "c"] for text "a b,c"', () => {
           const result = commaSeparatedToArray('a b, c')
-          expect(result).toStrictEqual(['a b', 'c'])
+          expect(result).toEqual(['a b', 'c'])
       })
       it('returns ["a", "b", "c"] for text "a, b,c"', () => {
           const result = commaSeparatedToArray('a, b,c')
-          expect(result).toStrictEqual(['a', 'b', 'c'])
+          expect(result).toEqual(['a', 'b', 'c'])
       })
-      it.todo('returns the array in ascending alphabetical order')
-      it.todo('contains each strings only once in the resulting array')
-      it.todo('returns all items in Titlecase')
+    })
+    describe('sortArrayAlphabetically', () => {
+      it('returns the array in ascending alphabetical order', () => {
+          const result = sortArrayAlphabetically('e,d,c,b,a')
+          expect(result).toEqual(['a', 'b', 'c', 'd', 'e'])
+      })
+    })
+    describe('reduceDublicates', () => {
+      it('contains each strings only once in the resulting array', () => {
+          const result = reduceDublicates('a,b,c,c,c,d,d,e,e')
+          expect(result).toBe(['a','b','c','d','e'])
+      })
+    })
+    describe('reduceDublicatesv2', () => {
+        it('contains each strings only once', () => {
+            const result = reduceDublicates('a,b,c,d,d,d,e,f,g,g,g')
+            expect(result).toBe(['a','b','c','d','e','f','g'])
+        })
+    })
+    describe('transformTitlecase', () => {
+      it('returns all items in Titlecase', () => {
+          const result = transformTitelcase('john', 'jane', 'pete')
+          expect(result).toEqual(['John', 'Jane', 'Pete'])
+      })
     })
 })
